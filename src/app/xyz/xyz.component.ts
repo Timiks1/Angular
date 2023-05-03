ed8880838d387f0117e4752b4a9b71531980f4d5
Timiks1 <prozor2@ukr.net>
2023-05-03 12:33:01 +0300

    nasa

---
+++ src/app/xyz/xyz.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './xyz.component.html',
+  styleUrls: ['./xyz.component.css']
+})
+export class XyzComponent {
+  public nasa!: Nasa[];
+  public currentIndex: number = 0;
+  public urls! : string[];
+  constructor(private http: HttpClient) {
+    this.urls = [];
+  }
+  ngOnInit() {
+    this.http.get('https://api.nasa.gov/planetary/apod?api_key=AbnXhIuJIedy22Mu18fd2qZDuc3yBcBFg7Ek1Bu2&count=100')
+      .subscribe(data => {
+        console.log(data)
+        let temp: string = JSON.stringify(data);
+        this.nasa = JSON.parse(temp);
+      });
+  }
+
+  public nextImage() {
+    this.urls = [];
+
+    if (this.currentIndex < this.nasa.length - 1) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
+   
+  }
+  public lastImage() {
+    this.urls = [];
+
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = this.nasa.length;
+    }
+  }
+  public showCopyright() {
+    this.urls = [];
+   this.nasa.forEach(element => {
+     if(element.copyright == this.nasa[this.currentIndex].copyright){
+        this.urls.push(element.hdurl);
+     }
+   });
+   
+  }
+}
+
+export class Nasa {
+  public copyright: string;
+  public date: string;
+  public explanation: string;
+  public hdurl: string;
+  public media_type: string;
+  public service_version: string;
+  public title: string;
+  public url: string;
+
+  constructor(c: string, d: string, ex: string, hdr: string, media: string, serv: string, title: string, url: string) {
+    this.copyright = c;
+    this.date = d;
+    this.explanation = ex;
+    this.hdurl = hdr;
+    this.media_type = media;
+    this.service_version = serv;
+    this.title = title;
+    this.url = url;
+  }
+}
